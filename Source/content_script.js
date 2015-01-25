d69b48f035b12747abcf87ce49f8b6ec04b7b148
Sacha Corazzi <sacha.corazzi@gmail.com>
2015-01-25 20:45:41 +0000

    Make 'Axel' >> 'Alex' extension

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,3 +1,4 @@
+console.log("Trollolololololollol");
 walk(document.body);
 
 function walk(node) 
@@ -31,10 +32,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bAxel\b/g, "Alex");
+	v = v.replace(/\baxel\b/g, "alex");
 	
 	textNode.nodeValue = v;
 }
